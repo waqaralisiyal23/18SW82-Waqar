46820ccc6c0c073f1d652cf31907ba320532e959
siraiwaqarali <49365563+siraiwaqarali@users.noreply.github.com>
2019-04-18 09:24:50 +0500

    Add files via upload

--- OOP-Lab3/ElectricityBill.java
+++ OOP-Lab3/ElectricityBill.java
@@ -1,28 +1,27 @@
-import java.util.*;
-public class ElectricityBill{
+import java.util.Scanner;
+class ElectricityBill{
 	public static void main(String[]args)
 	{
 		int units=0;
-		System.out.print("Enter number of units consumed: ");
+		System.out.print("Enter number of units: ");
 		Scanner sc=new Scanner(System.in);
 		units=sc.nextInt();
 		int price=0;
-		if(units<50){
+		if(units<=50){
 			price=units*10;
 		}
-		else if(units>=50 && units<=100){
-			price=units*15;
+		else if(units>50 && units<=100){
+			price = (50*10)+((units-50)*15);
 		}
 		else if(units>100 && units<=200){
-			price=units*20;
+			price = (50*10)+(50*15)+((units-100)*20);
 		}
 		else if(units>200 && units<=300){
-			price=units*25;
+			price = (50*10)+(50*15)+(100*20)+((units-200)*25);
 		}
 		else {
-			price=units*30;
+			price = (50*10)+(50*15)+(100*20)+(100*25)+((units-300)*30);
 		}
-		System.out.println("Total Electrictity Bill: "+ price + " Rupees");
-
+		System.out.println("Total Bill= "+ price +" Rupees");
 	}
 }
